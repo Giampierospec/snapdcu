a1398d89f9853b8cf2b56a02f44aeff15be2a1c9
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-09 15:38:01 -0400

    added new javascript and modified some css

--- js/snapPart.js
+++ js/snapPart.js
@@ -1,12 +1,121 @@
-var clsBtn = document.getElementById("clstab");
 var photoTake = document.getElementById("photo_button");
 var gallery = document.getElementById("gallery");
 var cam = document.getElementById("camera_take");
 var src = null;
 var width = 320;
-var height = 0;
+var height = 480;
 var streaming = false;
 var video = null;
 var canvas = null;
-var photo = null;
 var photo_button = null;
+var dat = [];
+var localStream;
+function photoSrc(src){
+  this.source = src;
+}
+function saveImage(src){
+  if(src != null){
+    var sc = new photoSrc(src);
+    dat.push(sc);
+    console.log(dat);
+    alert("Imagen guardada correctamente");
+    save();
+  }
+  else{
+    alert("La imagen no pudo ser guardada correctamente");
+  }
+
+}
+function save(){
+  var datos = JSON.stringify(dat);
+  localStorage.setItem("savedPhotos",datos);
+}
+function tomarFoto(){
+  $(".jumbotron").fadeIn("slow");
+  video = document.getElementById('video');
+  canvas = document.getElementById('canvas');
+  photo_button = document.getElementById('photo_button');
+  navigator.mediaDevices.getUserMedia({video:true, audio:false})
+    .then(function(stream){
+    video.srcObject = stream;
+    localStream = stream;
+    video.play();})
+    .catch(function(err){
+      console.log("An error occured! " + err);
+      alert("Hubo un error, se recargara la pagina");
+      location.reload();
+    });
+
+
+  video.addEventListener('canplay', function(ev){
+      if (!streaming) {
+        height = video.videoHeight / (video.videoWidth/width);
+
+        video.setAttribute('width', width);
+        video.setAttribute('height', height);
+        canvas.setAttribute('width', width);
+        canvas.setAttribute('height', height);
+        streaming = true;
+      }
+    }, false);
+
+    photo_button.addEventListener('click', function(ev){
+      takePic();
+      ev.preventDefault();
+    }, false);
+clearPhoto();
+}
+ function clearPhoto(){
+   var context = canvas.getContext('2d');
+   context.fillstyle = "#AAA";
+   context.fillRect(0,0,canvas.width, canvas.height);
+
+   var data = canvas.toDataURL('image/png');
+   src = data;
+ }
+ function takePic(){
+   var context = canvas.getContext('2d');
+   if(width && height){
+     canvas.width = width;
+     canvas.height = height;
+     context.drawImage(video,0,0, width,height);
+
+     var data = canvas.toDataURL('image/png');
+     src = data;
+      saveImage(src);
+   }
+   else{
+     clearPhoto();
+   }
+ }
+
+ function cargarVistaPrincipal(){
+   $(".jumbotron").hide();
+   var message = document.getElementById("message");
+   h1 = document.createElement("h1");
+   h1.innerHTML = "Recreando Vista Principal";
+   message.appendChild(h1);
+   $(h1).fadeOut(4000);
+   setTimeout(tomarFoto, 5000);
+ }
+ function loadGallery(){
+   if(confirm("Seguro que desea visitar la galeria?")){
+     $(".jumbotron").hide();
+     h1 = document.createElement("h1");
+     h1.innerHTML = "Redirigiendo a galeria";
+     message.appendChild(h1);
+     $(h1).addClass("animated fadeout infinite");
+     video.pause();
+  video.src = "";
+  localStream.getTracks()[0].stop();
+  console.log("Vid off");
+     setTimeout(redirectGallery,12000);
+
+   }
+ }
+ function redirectGallery(){
+   window.open("gallery.html","_self");
+ }
+ //Eventos
+ window.addEventListener("load",cargarVistaPrincipal);
+ gallery.addEventListener("click",loadGallery);
