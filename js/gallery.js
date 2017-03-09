d9a212328ede2b7e66e9e0d72588057505010bdf
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-09 18:10:17 -0400

    Added functions to gallery

--- js/gallery.js
+++ js/gallery.js
@@ -1,4 +1,5 @@
 var data = [];
+var btnBack = document.getElementById("btn_back");
 $(document).ready(loadData);
 
 function loadData(){
@@ -11,12 +12,26 @@ function loadData(){
 
 function output(){
   for(var i=0 ; i< data.length ; i++) {
-    $('<div class="item text-center"><img src="'+data[i]+'"></div>').appendTo('.carousel-inner');
+    $('<div class="item text-center"><img class="img-thumbnail" src="'+data[i]+'"></div>').appendTo('.carousel-inner');
     $('<li data-target="#galleryImages" data-slide-to="'+i+'"></li>').appendTo('.carousel-indicators')
 
   }
   $('.item').first().addClass('active');
   $('.carousel-indicators > li').first().addClass('active');
-  $('#galleryImages').carousel();
-  $("#galleryImages").interval(10000);
+  $('#galleryImages').carousel({interval: 5000});
 }
+
+function goBack(){
+  if (confirm("¿Seguro que desea regresar a la vista de fotos?")) {
+    $("#galleryImages").fadeOut(2000);
+    setTimeout(returnSnap, 8000);
+    h1 = document.createElement("h1");
+    h1.innerHTML = "Redirigiendo...";
+    $(h1).addClass("animated fadeOut infinite");
+    $(h1).appendTo(".container-fluid");
+  }
+}
+function returnSnap(){
+  window.open("snap.html","_self")
+}
+btnBack.addEventListener("click",goBack);
