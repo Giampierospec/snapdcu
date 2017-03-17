8358011247a70c89305c7220ea62a3beda84a6f6
Giampiero Specogna <giampi_12@hotmail.com>
2017-03-17 16:27:30 -0400

    added comments

--- js/gallery.js
+++ js/gallery.js
@@ -2,7 +2,7 @@ var data = [];
 var btnBack = document.getElementById("btn_back");
 $(document).ready(loadData);
 var i = 0;
-
+//Loads the data
 function loadData(){
   var datos = localStorage.getItem("savedPhotos");
   if(datos != null){
@@ -13,7 +13,7 @@ function loadData(){
     returnToSnap();
   }
 }
-
+//shows the output of the app
 function output(){
     if(i == data.length){
       i = 0;
@@ -23,13 +23,13 @@ function output(){
       $("#galleryImages").html("<div class='jumbotron thumbnail'><img src='"+data[i]+"'></div>").fadeIn(1000, fadeOutImage);
   }
 }
-
+//Fades out the images
 function fadeOutImage(){
   $("#galleryImages").fadeOut(7000);
   i++;
   setTimeout(output, 6000);
 }
-
+//Goes back to the image view
 function goBack(){
   if (confirm("¿Seguro que desea regresar a la vista principal?")) {
     $("#galleryImages").fadeOut(2000);
@@ -41,6 +41,7 @@ function goBack(){
     $(h1).appendTo(".container-fluid");
   }
 }
+//Returns to the view
 function returnToSnap(){
   $("#galleryImages").fadeOut(2000);
   $(btnBack).fadeOut(2000);
@@ -50,6 +51,7 @@ function returnToSnap(){
   $(h1).addClass("animated fadeOut infinite");
   $(h1).appendTo(".container-fluid");
 }
+//Returns to snap
 function returnSnap(){
   window.open("snap.html","_self")
 }
